4378a736c9c9028d8a66d63c5b605944ad79f5ad
francis.lovo <francis.lovo@externo.cresol.coop.br>
2023-03-15 10:42:15 -0300

    add args ao Botao

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { AbBotao } from '../src';
+import { AbBotao, AbBotaoProps } from '../src';
 
 export default {
   title: 'Componentes/AbBotao',
   component: AbBotao,
 } as ComponentMeta<typeof AbBotao>;
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />;
+const Template: ComponentStory<typeof AbBotao> = args => <AbBotao {...args} />;
 
 export const Primario = Template.bind({});
+Primario.args = {
+  texto: 'Primario',
+} as AbBotaoProps;
+
+export const Secundario = Template.bind({});
+Secundario.args = {
+  texto: 'Secundario',
+  tipo: 'secundario',
+} as AbBotaoProps;
+
+/*export const Primario = () => {
+  return <AbBotao texto="Primario" />;
+};
+export const Secundario = () => {
+  return <AbBotao texto="Secundario" tipo="secundario" />;
+};*/
